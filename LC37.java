9d0c43b8fb261fd427763edb6db3cc3f5cb1eaae
Haoran Li (Evan) <65979806+Haoranli802@users.noreply.github.com>
2024-03-21 09:52:19 +0800

    Create LC37.java

---
+++ LC37.java
@@ -0,0 +1,47 @@
+class Solution {
+    public void solveSudoku(char[][] board) {
+        solver(board);
+    }
+    private boolean solver(char[][] board){
+        for(int i = 0; i < board.length; i++){
+            for(int j = 0; j < board[0].length; j++){
+                if(board[i][j] == '.'){
+                    for(char n = '1'; n <= '9'; n++){
+                        if(isValidSudoku(i, j, n, board)){
+                            board[i][j] = n;
+                            if(solver(board)) return true;
+                            board[i][j] = '.';
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    private boolean isValidSudoku(int row, int col, char val, char[][] board){
+        // 同行是否重复
+        for (int i = 0; i < 9; i++){
+            if (board[row][i] == val){
+                return false;
+            }
+        }
+        // 同列是否重复
+        for (int j = 0; j < 9; j++){
+            if (board[j][col] == val){
+                return false;
+            }
+        }
+        // 9宫格里是否重复
+        int startRow = (row / 3) * 3;
+        int startCol = (col / 3) * 3;
+        for (int i = startRow; i < startRow + 3; i++){
+            for (int j = startCol; j < startCol + 3; j++){
+                if (board[i][j] == val){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
